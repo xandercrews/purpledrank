f48c8e9d45a4cf8d91f20d94cf52d996cae80fb2
Jonathan Dye <jonathan.dye@sungard.com>
2014-04-30 09:47:13 -0600

    deployment partial work

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,10 +1,14 @@
 from fabric.api import run, cd, env, settings, prefix
+from fabric.context_managers import shell_env
 import os
 import yaml
 
 REPO_TOPDIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 
 REPO_URL="git@github.com:xandercrews/purpledrank.git"
+
+PID_FILE = "conf/supervisord.pid"
+SUPERVISOR_CONF = 'conf/supervisor.conf'
 
 env.use_ssh_config = True
 env.forward_agent = True
@@ -42,16 +46,30 @@ def deploy(gittag=None):
     if new_venv:
         run('virtualenv %s -p `which python2.7`' % venv_dir)
 
-    config = get_config()
-    assert 'common' in config, 'config must have common section'
-    assert 'magicport' in config['common'], 'config must have magic port in common section'
+    supervisor_running = True
+    with cd(code_dir):
+        with settings(warn_only=True):
+            if run('[ -f %s ]' % PID_FILE).failed:
+                supervisor_running = False
+            if run('read PID < %s; [ -d /proc/${PID} ]' % PID_FILE).failed:
+                supervisor_running = False
 
-    magic_port = config['common']['magicport']
+    if not supervisor_running:
+        # run supervisor
+        with prefix('. %s/bin/activate' % venv_dir.strip('/')):
+            with shell_env(PYTHONPATH=code_dir):
+                assert 'Python 2.7' in run('python --version'), 'require python 2.7'
+                run('supervisord -c %s' % SUPERVISOR_CONF)
+
+    # config = get_config()
+    # assert 'common' in config, 'config must have common section'
+    # assert 'magicport' in config['common'], 'config must have magic port in common section'
+    # magic_port = config['common']['magicport']
 
     # activate venv
-    with cd(code_dir):
-        with prefix('. %s/bin/activate' % venv_dir):
-            assert 'Python 2.7' in run('python --version'), 'require python 2.7'
-            # TODO install deps with setup.py
-            # run magic service
-            run('zerorpc --bind tcp://0.0.0.0:%d purpledrank.services.magic.MagicService' % magic_port)
+    # with prefix('. %s/bin/activate' % venv_dir.strip('/')):
+    #     with shell_env(PYTHONPATH=code_dir):
+    #         assert 'Python 2.7' in run('python --version'), 'require python 2.7'
+    #         TODO install deps with setup.py
+    #         run supervisor
+    #         run('PYTHONPATH=%s zerorpc --bind tcp://0.0.0.0:%d purpledrank.services.magic.MagicService' % (magic_port))
