cc84190de198c94b6c843390ef01fa9e728f586f
Jonathan Dye <jonathan.dye@sungard.com>
2014-04-30 10:29:03 -0600

    fix test streaming service typo

--- purpledrank/services/test/__init__.py
+++ purpledrank/services/test/__init__.py
@@ -1,9 +1,11 @@
 __author__ = 'achmed'
 
 from ..baseservice import BaseService
+import zerorpc
 import gevent
 
 class TestService(BaseService):
+    @zerorpc.stream
     def stream_forever(self):
         while True:
             gevent.sleep(1)
