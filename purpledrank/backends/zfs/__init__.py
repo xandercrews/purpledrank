9f90e3af90b610adb054dd7ea8017dc3d3f9afe8
Jonathan Dye <jonathan.dye@sungard.com>
2014-05-01 20:09:40 -0600

    zpool parse debug

--- purpledrank/backends/zfs/__init__.py
+++ purpledrank/backends/zfs/__init__.py
@@ -12,9 +12,17 @@ else:
     assert sys.platform.startswith('linux')
     ZPOOL_CMD = '/usr/bin/zpool'
 
+from ...log import init_logger
+init_logger()
+
+import logging
+logger = logging.getLogger()
+
 def get_zpool_status(self):
     p = subprocess.Popen([ZPOOL_CMD, 'status',], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=None)
     out, err = p.communicate()
+    logger.debug('zpool status output\n' + out)
+
     if p.returncode != 0:
         raise Exception('zpool status command failed: %s' % err)
 
@@ -24,6 +32,6 @@ def get_zpool_status(self):
     if r is None:
         raise Exception('unknown problem parsing zpool status')
 
+    logger.debug('parser remainder\n' + parser.remainder())
+
     return util.get_zpool_tree(r)
-
-
